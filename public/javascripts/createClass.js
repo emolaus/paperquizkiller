4434dde639e105de64b51f200a7e6b7e1aa0aa15
Mattias Olausson <bakaren@gmail.com>
2015-10-21 16:23:40 +0200

    v.4, WIP task 51. Enter pres adds student to list.

--- public/javascripts/createClass.js
+++ public/javascripts/createClass.js
@@ -1,5 +1,16 @@
 (function () {
   var app = angular.module('createClass', []);
-  app.controller('MainController', ['$scope', '$http', '$window', function ($scope, $http, $window){
+  app.controller('MainController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+    $scope.nameOfClass = "";
+    $scope.studentList = [];
+    $scope.studentNameEntered = function () {
+      if (!$scope.newStudentName) return;
+      $scope.studentList.push($scope.newStudentName);
+      console.log($scope.studentList);
+      $scope.newStudentName = ''; 
+    }
+    $scope.saveClass = function () {
+      console.log('Its happeniiiing!! ');
+    }
   }]);
 })();
