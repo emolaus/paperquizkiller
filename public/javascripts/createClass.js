8516de8e161268a823cc62b7121112a1ea9a8705
Mattias Olausson <bakaren@gmail.com>
2015-10-21 17:36:59 +0200

    v.4, WIP task 51. Checks uniqueness of name

--- public/javascripts/createClass.js
+++ public/javascripts/createClass.js
@@ -6,13 +6,13 @@
     $scope.infoText = 'Kaka'
     $scope.studentNameEntered = function () {
       if (!$scope.newStudentName) return;
-      if ($scope.studentList.length == 0) {
-        $scope.infoText = "You need to add at least one student.";
+      if ($scope.studentList.length >= 5) {
+        $scope.infoText = "You've reached the maximum nr of students in one class.";
         $('#modalInfo').modal('show');
         return;
       }
-      if ($scope.studentList.length >= 5) {
-        $scope.infoText = "You've reached the maximum nr of students in one class.";
+      if ($scope.studentList.indexOf($scope.newStudentName) > -1) {
+        $scope.infoText = "Your student names must be unique. Perhaps add a nickname, lastname or initial.";
         $('#modalInfo').modal('show');
         return;
       }
@@ -20,6 +20,11 @@
       $scope.newStudentName = ''; 
     }
     $scope.saveClass = function () {
+      if ($scope.studentList.length == 0) {
+        $scope.infoText = "You need to add at least one student.";
+        $('#modalInfo').modal('show');
+        return;
+      }
       if (!$scope.nameOfClass) {
         $scope.infoText = 'Please select a name for this class.';
         $('#modalInfo').modal('show');
